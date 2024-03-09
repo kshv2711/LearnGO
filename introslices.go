f00d993de9f677d2413da54943f8a2961f21f520
kshv2711 <keshavsharma2711@gmail.com>
2024-03-10 00:22:58 +0530

    introduction of slices

---
+++ introslices.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	
+)
+func main(){
+	number:=[]int{1,2,3,4,5,6,7,8,9}
+
+	// using  for loop
+	// for i:=0;i<len(number);i++{
+	// 	fmt.Println(number[i])
+	// }
+
+
+	// using for range loop
+	// for i, eln:=range number{
+	// 	fmt.Printf("index of slice %v value od slice is %v\n",i,eln)	
+	// }
+
+
+	//append element in slice
+	// fmt.Println(len(number))//before the append
+	// fmt.Println(number)
+	// number =append(number, 10,11,12,13,14,15)
+	// fmt.Println(len(number))//after the append
+	// fmt.Println(number)
+
+
+	//append the one slice to another slice 
+    // number2:=[]int{67,878,878,}
+	// number=append(number,number2...)
+	// fmt.Println(number)
+
+
+	//copy slice 
+	// demo:=make([]int,len(number))
+	//
+	//n:= copy(demo,number)
+	// fmt.Println(number,demo,n)
+	// fmt.Printf("%T",n)// n is total number of element in copied slices
+
+	//compare slice
+	var k bool = true
+	number3:=[]int{1,2,3,4,5,6,7,8,9}
+	for i, val :=range number{
+		if val!=number3[i]{
+			k=false
+			break
+
+
+		}
+		
+	}
+	if k {
+		fmt.Println("equal")
+	}else{
+		fmt.Println("not")
+	}
+
+	
+
+}
