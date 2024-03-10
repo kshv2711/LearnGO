8f5b3ccae1bbe8e19728514f9db901af554216f7
kshv2711 <keshavsharma2711@gmail.com>
2024-03-10 16:48:41 +0530

    change number to number1

--- Slices/mySlices.go
+++ Slices/mySlices.go
@@ -5,10 +5,10 @@ import (
 	
 )
 func main(){
-	number:=[]int{1,2,3,4,5,6,7,8,9}
+	number1:=[]int{1,2,3,4,5,6,7,8,9}
 
 	//using  for loop
-	for i:=0;i<len(number);i++{
-		fmt.Println(number[i])
+	for i:=0;i<len(number1);i++{
+		fmt.Println(number1[i])
 	}
 }
